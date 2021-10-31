e25a75fac16483e16452c32f2a189ba3742e389f
verhovensky <voinywolnyprod@gmail.com>
2021-10-31 13:49:15 +0300

    Cart views.py formatting fix

--- cart/views.py
+++ cart/views.py
@@ -11,15 +11,10 @@ def cart_add(request, product_id):
     cart = Cart(request)
     product = get_object_or_404(Product, id=product_id)
     form = CartAddProductForm(request.POST)
-    # Stock product quantity check
-    # All values converted to Decimal for calculation
     quantity_added_by_user = Decimal(request.POST.get('quantity'))
-    # print("Add by user: ", type(quantity_added_by_user), quantity_added_by_user)
-    # print("Product stock: ", type(product.stock), product.stock)
     if quantity_added_by_user >= product.stock or quantity_added_by_user <= 0:
-        message = "{stock} на складе {name}, но {ordered} заказанно!".format(stock=product.stock,
-                                                                             ordered=quantity_added_by_user,
-                                                                             name=product.name)
+        message = f"{product.stock} на складе товара {product.name}, " \
+                  f"но {quantity_added_by_user} заказанно!"
         page_title = 'Ошибка заказа'
         header = 'Ошибка заказа'
         return render(request, 'cart/detail.html', {'cart': cart,
