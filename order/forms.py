bd330b4c903261cd80b3f3b1803bf1b522e10819
verhovensky <voinywolnyprod@gmail.com>
2021-12-21 23:24:06 +0300

    Order delivery_time validation fix

--- order/forms.py
+++ order/forms.py
@@ -1,15 +1,26 @@
 from django import forms
 from .models import Order
 from client.models import Client
-from django.core.validators import RegexValidator
+from django.core.validators import RegexValidator, MinValueValidator
 from bootstrap_datepicker_plus import DateTimePickerInput
+from django.utils import timezone
 
-
+# Phone validation
 phone_regex = RegexValidator(regex=r'^\+?1?\d{9,16}$',
                              message="Введите номер телефона, макс. длинна = 16 знаков")
 
+# Delivery time validation
+no_past_date_validator = MinValueValidator(limit_value=timezone.localtime(),
+                                           message='Нельзя указать дату в прошлом!')
+
 
 class OrderCreateFormForNewCustomer(forms.ModelForm):
+
+    class Meta:
+        model = Order
+        fields = ['full_name', 'phone', 'address', 'delivery_time',
+                  'self_pick', 'cash_on_delivery', 'description']
+
     full_name = forms.CharField(label="",
                                 help_text="",
                                 min_length=3,
@@ -31,12 +42,14 @@ class OrderCreateFormForNewCustomer(forms.ModelForm):
                               (attrs={'class': 'form-control',
                                       'placeholder': 'Адрес доставки'}))
     delivery_time = forms.DateTimeField(label="",
-                              help_text="",
-                              widget=DateTimePickerInput
-                              (attrs={'class': 'form-control datetimepicker-input',
-                                      'data-target': '#datetimepicker1',
-                                      'placeholder': 'Время доставки',
-                                      'format': '%m/%d/%Y'}))
+                                        validators=[no_past_date_validator],
+                                        help_text="",
+                                        input_formats=['%m/%d/%Y %H:%M'],
+                                        widget=DateTimePickerInput
+                                        (attrs={'class': 'form-control datetimepicker-input',
+                                                'data-target': '#datetimepicker1',
+                                                'placeholder': 'Время доставки',
+                                                'format': '%m/%d/%Y %H:%M'}))
 
     description = forms.CharField(label="",
                                   help_text="",
@@ -46,11 +59,6 @@ class OrderCreateFormForNewCustomer(forms.ModelForm):
                                   (attrs={'class': 'form-control',
                                           'placeholder': 'Другие примечания / пожелания',
                                           'width': "50%", 'cols': "60", 'rows': "10", }))
-
-    class Meta:
-        model = Order
-        fields = ['full_name', 'phone', 'address', 'delivery_time',
-                  'self_pick', 'cash_on_delivery', 'description']
 
 
 class OrderCreateFormForExistingCustomer(forms.ModelForm):
@@ -79,20 +87,22 @@ class OrderCreateFormForExistingCustomer(forms.ModelForm):
                                       'placeholder': 'Адрес доставки'}))
 
     name = forms.CharField(label="",
-                              help_text="",
-                              min_length=8,
-                              max_length=250,
-                              required=False,
-                              widget=forms.HiddenInput())
+                           help_text="",
+                           min_length=8,
+                           max_length=250,
+                           required=False,
+                           widget=forms.HiddenInput())
 
     delivery_time = forms.DateTimeField(label="",
-                              help_text="",
-                              required=True,
-                              widget=DateTimePickerInput
-                              (attrs={'class': 'form-control datetimepicker-input',
-                                      'data-target': '#datetimepicker1',
-                                      'placeholder': 'Время доставки',
-                                      'format': '%m/%d/%Y'}))
+                                        help_text="",
+                                        validators=[no_past_date_validator],
+                                        required=True,
+                                        input_formats=['%m/%d/%Y %H:%M'],
+                                        widget=DateTimePickerInput
+                                        (attrs={'class': 'form-control datetimepicker-input',
+                                                  'data-target': '#datetimepicker1',
+                                                  'placeholder': 'Время доставки',
+                                                  'format': '%m/%d/%Y %H:%M'}))
 
     description = forms.CharField(label="",
                                   help_text="",
--- order/models.py
+++ order/models.py
@@ -2,10 +2,7 @@ from django.db import models
 from product.models import Product
 from client.models import Client
 from account.models import UserProfile
-# Decimal
 import decimal
-from django.core.validators import MinValueValidator
-from django.utils import timezone
 
 
 class Order(models.Model):
@@ -33,24 +30,20 @@ class Order(models.Model):
     phone = models.CharField(max_length=22, null=True, blank=True)
     address = models.CharField(max_length=250, verbose_name="Адрес доставки")
     postal_code = models.CharField(max_length=20, null=True, blank=True, verbose_name="Почтовый индекс")
-    # Client.client_orders.all()
-    # Client associated with the Order, can be null and blank for comfort
     this_order_client = models.ForeignKey(Client, related_name='client_orders', null=True, blank=True,
                                           verbose_name="От клиента", on_delete=models.SET_NULL)
-    # Account associated with this order, who created order
     this_order_account = models.ForeignKey(UserProfile, related_name='user_orders', null=True,
                                            verbose_name="Создавший", on_delete=models.SET_NULL)
     status = models.CharField(choices=STATUSORD, default=PENDING, max_length=16, blank=False,
                               verbose_name="Статус")
     # TODO: unique = True, error "unique":""
-    delivery_time = models.DateTimeField(validators=[MinValueValidator(limit_value=timezone.now())],
-                                         verbose_name="Время доставки",
-                                         error_messages={"blank": "Выберите дату заказа",
-                                                         "null": "Выберите реальную дату заказа",
-                                                         "invalid_choice": "Нельзя создать заказ в прошлом!",
-                                                         "invalid": "Нельзя создать заказ в прошлом!"})
+    delivery_time = models.DateTimeField(verbose_name="Время доставки", unique=True,
+                                         error_messages={"blank": "Выберите дату заказа"})
     self_pick = models.BooleanField(null=False, default=False, verbose_name="Самовывоз")
     cash_on_delivery = models.BooleanField(null=False, default=False, verbose_name="Оплата при получении")
+    # TODO: total value of order, save from view (cart)
+    # total = models.DecimalField(max_digits=10, decimal_places=1, validators=[MinValueValidator(Decimal('300.0'))],
+    #                                 verbose_name='Сумма заказа')
     created = models.DateTimeField(auto_now_add=True, verbose_name="Дата оформления")
     updated = models.DateTimeField(auto_now=True, verbose_name="Статус изменен")
     description = models.TextField(blank=True, max_length=500, verbose_name="Примечание")
