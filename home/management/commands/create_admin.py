4c5fda45d3ab588ce213a4e35f5b4967e80e5492
verhovensky <voinywolnyprod@gmail.com>
2022-04-24 19:17:10 +0300

    Add create_admin.py

---
+++ home/management/commands/create_admin.py
@@ -0,0 +1,37 @@
+import os
+import random
+import string
+from product.models import Product
+from client.models import Client
+from order.models import Order, OrderItem
+from django.contrib.auth.models import User
+from django.core.management.base import BaseCommand
+from django.contrib.auth.models import Group, Permission
+
+
+class Command(BaseCommand):
+    def __init__(self, *args, **kwargs):
+        super(Command, self).__init__(*args, **kwargs)
+
+    def handle(self, *args, **options):
+        pswd = ''.join(random.SystemRandom().choice(string.ascii_letters + string.digits) for _ in range(10))
+        admin = User.objects.create_superuser(username='BaseAdmin',
+                                              email=os.getenv('ADMIN_EMAIL'),
+                                              password=pswd)
+        try:
+            admins_group = Group.objects.get(name='Admins')
+            admins_group.user_set.add(admin)
+        except Group.DoesNotExist:
+            print(f"Group Admins does not exist! Creating...")
+            admins_group = Group.objects.create(name='Admins')
+            for model in [Client, Product, Order, OrderItem]:
+                for perm in ['add', 'change', 'delete', 'view']:
+                    codename = perm + "_" + model._meta.model_name
+                    permission = Permission.objects.get(codename=codename)
+                    admins_group.permissions.add(permission)
+                    print(f"Add permission {perm} _{model._meta.model_name} to Admins group")
+            admins_group.user_set.add(admin)
+        finally:
+            print(f"Admin created\n"
+                  f"username: BaseAdmin\n"
+                  f"password: {pswd}")
