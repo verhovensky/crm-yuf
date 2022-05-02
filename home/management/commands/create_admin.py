d8762a44a1755f1895eff4a6b7bbb24212cfab2e
verhovensky <voinywolnyprod@gmail.com>
2022-05-02 17:38:03 +0300

    create_admin.py fix

--- home/management/commands/create_admin.py
+++ home/management/commands/create_admin.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import random
 import string
 from product.models import Product
@@ -14,6 +15,9 @@ class Command(BaseCommand):
         super(Command, self).__init__(*args, **kwargs)
 
     def handle(self, *args, **options):
+        if User.objects.filter(username='BaseAdmin').exists():
+            self.stdout.write('User BaseAdmin already exists')
+            sys.exit(0)
         pswd = ''.join(random.SystemRandom().choice(string.ascii_letters + string.digits) for _ in range(10))
         admin = User.objects.create_superuser(username='BaseAdmin',
                                               email=os.getenv('ADMIN_EMAIL'),
@@ -22,16 +26,16 @@ class Command(BaseCommand):
             admins_group = Group.objects.get(name='Admins')
             admins_group.user_set.add(admin)
         except Group.DoesNotExist:
-            print(f"Group Admins does not exist! Creating...")
+            self.stdout.write(f"Group Admins does not exist! Creating...")
             admins_group = Group.objects.create(name='Admins')
             for model in [Client, Product, Order, OrderItem]:
                 for perm in ['add', 'change', 'delete', 'view']:
                     codename = perm + "_" + model._meta.model_name
                     permission = Permission.objects.get(codename=codename)
                     admins_group.permissions.add(permission)
-                    print(f"Add permission {perm} _{model._meta.model_name} to Admins group")
+                    self.stdout.write(f"Add permission {perm} _{model._meta.model_name} to Admins group")
             admins_group.user_set.add(admin)
         finally:
-            print(f"Admin created\n"
-                  f"username: BaseAdmin\n"
-                  f"password: {pswd}")
+            self.stdout.write(f"Admin created "
+                              f"username: BaseAdmin "
+                              f"password: {pswd}")
