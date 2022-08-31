7d0161f0735c7cf9ceff20a8bb29b68b7682dfef
verhovensky <voinywolnyprod@gmail.com>
2022-08-31 14:33:53 +0300

    urls.py refactoring, formatting fixes

--- order/urls.py
+++ order/urls.py
@@ -1,27 +1,31 @@
 from django.urls import path
-from .views import OrderListAll, ChangeOrder, CreateExists, OrderDetailView, DeleteOrder
+from .views import OrderListAll, ChangeOrder, OrderCreateView, OrderDetailView, DeleteOrder
 from django.conf import settings
 from django.conf.urls.static import static
 
-app_name = 'order'
+app_name = "order"
 
 urlpatterns = [
-    path('', OrderListAll.as_view(extra_context=
-                                      {'page_title': 'Заказы',
-                                       'page_header': 'Все заказы'}),
-         name='list'),
-    path('detail/<int:pk>', OrderDetailView.as_view(extra_context=
-                                                    {'page_title': 'Детали заказа',
-                                                     'page_header': 'Детали заказа'}),
-         name='detail'),
-    path('create/exists', CreateExists.as_view(extra_context=
-                                                {'page_title': 'Создание заказа',
-                                                 'page_header': 'Новый заказ'}),
-         name='create_exists'),
-    path('change/<int:pk>', ChangeOrder.as_view(extra_context=
-                                       {'page_title': 'Изменить статус',
-                                        'page_header': 'Изменить статус заказа'}),
-         name='order_change'),
-    path('delete/<int:pk>', DeleteOrder.as_view(),
-         name='order_delete')
+    path("", OrderListAll.
+         as_view(extra_context={
+                "page_title": "Заказы",
+                "page_header": "Все заказы"}),
+         name="list"),
+    path("detail/<int:pk>", OrderDetailView.
+         as_view(extra_context={
+                 "page_title": "Детали заказа",
+                 "page_header": "Детали заказа"}),
+         name="detail"),
+    path("create/", OrderCreateView.
+         as_view(extra_context={
+                 "page_title": "Создание заказа",
+                 "page_header": "Новый заказ"}),
+         name="create"),
+    path("change/<int:pk>", ChangeOrder.
+         as_view(extra_context={
+                 "page_title": "Изменить статус",
+                 "page_header": "Изменить статус заказа"}),
+         name="change"),
+    path("delete/<int:pk>", DeleteOrder.as_view(),
+         name="delete")
 ] + static(settings.STATIC_URL)
