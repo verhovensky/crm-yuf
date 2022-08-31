c53ba40e596be9e797befa06ebabf661f8c8d430
verhovensky <voinywolnyprod@gmail.com>
2022-08-31 16:56:44 +0300

    cart.py cart class refactor, data types fixes

--- cart/cart.py
+++ cart/cart.py
@@ -1,7 +1,4 @@
-from decimal import Decimal
 import decimal
-import json
-from django.core.serializers.json import DjangoJSONEncoder
 from django.conf import settings
 from product.models import Product
 
@@ -17,64 +14,66 @@ class Cart(object):
         if not cart:
             cart = self.session[settings.CART_SESSION_ID] = {}
         self.cart = cart
+        self.id_to_product()
         self._data_len = len(self.cart.items())
 
-    def save(self):
-        # Session refresh cart
-        self.session[settings.CART_SESSION_ID] = self.cart
-        self.session.modified = True
-
-    def add(self, product, quantity=1, update_quantity=False):
-        """
-        Add product to cart
-        """
-        if str(product.id) not in self.cart:
-            self.cart[str(product.id)] = {'quantity': decimal.Decimal(quantity),
-                                          'price': str(product.price)}
-        if update_quantity:
-            self.cart[str(product.id)]['quantity'] = decimal.Decimal(self.cart[str(product.id)]['quantity']) \
-                                                     + decimal.Decimal(quantity)
-        self.cart[str(product.id)]['quantity'] = json.dumps(self.cart[str(product.id)]['quantity'].
-                                                            quantize(Decimal('.01'),
-                                                            rounding=decimal.ROUND_DOWN),
-                                                            cls=DjangoJSONEncoder).replace('"', '')
-        self.save()
-
-    def remove(self, product):
-        """
-        Remove products from cart
-        """
-        product.stock = product.stock + Decimal(self.cart[str(product.id)]['quantity'])
-        product.save()
-        del self.cart[str(product.id)]
-        self.save()
-
     def __iter__(self):
-        product_ids = self.cart.keys()
-        products = Product.objects.filter(id__in=product_ids)
-        for product in products:
-            self.cart[str(product.id)]['product'] = product
-
         for item in self.cart.values():
-            item_total_price = decimal.Decimal(item['price']) * \
-                               decimal.Decimal(item['quantity'].replace('"', ''))
-            item['total_price'] = json.dumps(item_total_price.quantize(Decimal('.01'),
-                                             rounding=decimal.ROUND_DOWN),
-                                             cls=DjangoJSONEncoder).replace('"', '')
+            item["total_price"] = \
+                decimal.Decimal(item["price"]) \
+                * decimal.Decimal(item["quantity"])
+            item["total_price"] = str(item["total_price"])
             yield item
 
     def __len__(self):
         return self._data_len
 
+    def id_to_product(self):
+        """ Convert ids of products to product instances
+        However, this may be redundant in DRF
+        with serializers (ex. queryset param) """
+        product_ids = self.cart.keys()
+        products = Product.objects.\
+            filter(id__in=product_ids).\
+            only("pk", "name", "price", "stock")
+        for product in products:
+            self.cart[str(product.id)]["product"] = product
+
     def get_total_price(self):
+        """ Sum up prices of all products in cart """
         at = []
         for i, x in enumerate(self.cart.values()):
-            tp = decimal.Decimal(x['price']) * \
-                               decimal.Decimal(x['quantity'])
+            tp = decimal.Decimal(x["price"]) * \
+                               decimal.Decimal(x["quantity"])
             at.append(tp)
-        return sum(at)
+        return str(sum(at))
+
+    def save(self):
+        """ Save modified cart content in session """
+        self.session[settings.CART_SESSION_ID] = self.cart
+        self.session.modified = True
+
+    def add(self, product, quantity, update_quantity=False):
+        # TODO: update product quantity method
+        """ Add product to cart """
+        if str(product.id) not in self.cart:
+            self.cart[str(product.id)] = {
+                "quantity": str(quantity),
+                "price": str(product.price)}
+        if update_quantity:
+            upd = decimal.Decimal(
+                self.cart[str(product.id)]["quantity"]) \
+                  + decimal.Decimal(quantity)
+            self.cart[str(product.id)]["quantity"] = str(upd)
+        self.save()
+
+    def remove(self, product):
+        # TODO: substract product quantity method
+        """ Remove product from cart """
+        del self.cart[str(product.id)]
+        self.save()
 
     def clear(self):
-        # удаление корзины из сессии
+        """ Remove cart instance from session """
         del self.session[settings.CART_SESSION_ID]
         self.session.modified = True
