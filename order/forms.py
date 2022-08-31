b879479746c86058d8bcb1e6367ad86643e5abf1
verhovensky <voinywolnyprod@gmail.com>
2022-08-31 14:18:48 +0300

    forms.py refactor, single form for order creation

--- order/forms.py
+++ order/forms.py
@@ -1,130 +1,179 @@
 from django import forms
-from .models import Order
+from order.models import Order
 from client.models import Client
-from django.core.validators import RegexValidator, MinValueValidator
+from django.core.validators import MinValueValidator, RegexValidator
 from bootstrap_datepicker_plus.widgets import DateTimePickerInput
 from django.utils import timezone
 
-# Phone validation
-phone_regex = RegexValidator(regex=r'^\+?1?\d{9,16}$',
-                             message="Введите номер телефона, макс. длинна = 16 знаков")
-
 # Delivery time validation
-no_past_date_validator = MinValueValidator(limit_value=timezone.localtime(),
-                                           message='Нельзя указать дату в прошлом!')
+no_past_date_validator = MinValueValidator(
+    limit_value=timezone.localtime(),
+    message="Нельзя указать дату в прошлом!")
 
 
-class OrderCreateFormForNewCustomer(forms.ModelForm):
+class OrderCreateForm(forms.ModelForm):
 
-    class Meta:
-        model = Order
-        fields = ['full_name', 'phone', 'address', 'delivery_time',
-                  'self_pick', 'cash_on_delivery', 'description']
-
-    full_name = forms.CharField(label="",
-                                help_text="",
-                                min_length=3,
-                                max_length=22,
-                                widget=forms.TextInput
-                                (attrs={'class': 'form-control',
-                                        'placeholder': 'ФИО заказчика'}))
-    phone = forms.CharField(label="",
-                            help_text="",
-                            validators=[phone_regex],
-                            widget=forms.NumberInput
-                            (attrs={'class': 'form-control',
-                                    'placeholder': 'Моб. телефон'}))
-    address = forms.CharField(label="",
-                              help_text="",
-                              min_length=3,
-                              max_length=250,
-                              widget=forms.TextInput
-                              (attrs={'class': 'form-control',
-                                      'placeholder': 'Адрес доставки'}))
-    delivery_time = forms.DateTimeField(label="",
-                                        validators=[no_past_date_validator],
-                                        help_text="",
-                                        input_formats=['%m/%d/%Y %H:%M'],
-                                        widget=DateTimePickerInput
-                                        (attrs={'class': 'form-control datetimepicker-input',
-                                                'data-target': '#datetimepicker1',
-                                                'placeholder': 'Время доставки',
-                                                'format': '%m/%d/%Y %H:%M'}))
-
-    description = forms.CharField(label="",
-                                  help_text="",
-                                  max_length=500,
-                                  required=False,
-                                  widget=forms.Textarea
-                                  (attrs={'class': 'form-control',
-                                          'placeholder': 'Другие примечания / пожелания',
-                                          'width': "50%", 'cols': "60", 'rows': "10", }))
-
-
-class OrderCreateFormForExistingCustomer(forms.ModelForm):
+    new_client = forms.BooleanField(
+        label="Новый клиент",
+        required=False,
+        widget=forms.CheckboxInput(
+            attrs={"placeholder": "Новый клиент",
+                   "class": "new-client"}))
 
     class Meta:
         model = Order
-        fields = ['this_order_client', 'address', 'delivery_time',
-                  'self_pick', 'cash_on_delivery', 'description']
+        fields = ("new_client", "for_other",
+                  "this_order_client", "full_name",
+                  "address", "phone", "delivery_time",
+                  "self_pick", "cash_on_delivery",
+                  "description")
 
-    this_order_client = forms.ModelChoiceField(label="Заказ клиента:",
-                                               queryset=Client.objects.all().order_by('name'),
-                                               initial=0,
-                                               required=True,
-                                               help_text="",
-                                               widget=forms.Select
-                                               (attrs={'class': 'form-control text-input',
-                                                       'placeholder': 'Выберите заказчика'}))
+    this_order_client = forms.ModelChoiceField(
+        label="Выберите клиента:",
+        queryset=Client.objects.
+            only("name", "pk", "phone_number").
+            order_by("name"),
+        initial=0,
+        required=False,
+        help_text="",
+        widget=forms.Select(
+            attrs={"class": "form-control selected-client",
+                   "placeholder": "Выберите заказчика"}))
 
-    address = forms.CharField(label="",
-                              help_text="",
-                              min_length=8,
-                              max_length=250,
-                              required=True,
-                              widget=forms.TextInput
-                              (attrs={'class': 'form-control',
-                                      'placeholder': 'Адрес доставки'}))
+    address = forms.CharField(
+        label="",
+        help_text="",
+        min_length=8,
+        max_length=250,
+        required=False,
+        widget=forms.TextInput(
+            attrs={"class": "form-control new-address",
+                   "placeholder": "Адрес доставки"}))
 
-    name = forms.CharField(label="",
-                           help_text="",
-                           min_length=8,
-                           max_length=250,
-                           required=False,
-                           widget=forms.HiddenInput())
+    phone = forms.CharField(
+        label="",
+        help_text="",
+        required=False,
+        validators=[
+            RegexValidator(
+                regex=r"^\+?1?\d{9,16}$",
+                message="Телефон должен быть в формате: +700112299"
+                        " Макс. длинна = 16 знаков")],
+        widget=forms.TextInput(
+            attrs={"class": "form-control new-phone",
+                   "placeholder": "Телефон получателя"}))
 
-    delivery_time = forms.DateTimeField(label="",
-                                        help_text="",
-                                        validators=[no_past_date_validator],
-                                        required=True,
-                                        input_formats=['%m/%d/%Y %H:%M'],
-                                        widget=DateTimePickerInput
-                                        (attrs={'class': 'form-control datetimepicker-input',
-                                                  'data-target': '#datetimepicker1',
-                                                  'placeholder': 'Время доставки',
-                                                  'format': '%m/%d/%Y %H:%M'}))
+    full_name = forms.CharField(
+        label="",
+        help_text="",
+        min_length=3,
+        max_length=32,
+        required=False,
+        widget=forms.TextInput(
+            attrs={"class": "form-control new-fullname",
+                   "placeholder": "Имя получателя"}))
 
-    description = forms.CharField(label="",
-                                  help_text="",
-                                  max_length=500,
-                                  required=False,
-                                  widget=forms.Textarea
-                                  (attrs={'class': 'form-control',
-                                          'placeholder': 'Другие примечания / пожелания',
-                                          'width': "50%", 'cols': "60", 'rows': "10", }))
+    delivery_time = forms.DateTimeField(
+        label="",
+        help_text="",
+        validators=[no_past_date_validator],
+        required=True,
+        input_formats=["%m/%d/%Y %H:%M"],
+        widget=DateTimePickerInput(attrs={
+            "class": "form-control datetimepicker-input",
+            "data-target": "#datetimepicker1",
+            "placeholder": "Время доставки",
+            "format": "%m/%d/%Y %H:%M"}))
+
+    description = forms.CharField(
+        label="",
+        help_text="",
+        max_length=500,
+        required=False,
+        widget=forms.Textarea(
+            attrs={"class": "form-control",
+                   "placeholder":
+                       "Другие примечания / пожелания",
+                   "width": "50%", "cols": "60",
+                   "rows": "10", }))
+
+    def clean(self):
+        cleaned_data = super().clean()
+        if cleaned_data["for_other"]:
+            self.required_fields(["full_name", "address",
+                                  "phone", "this_order_client"])
+        if cleaned_data["new_client"]:
+            self.required_fields(["full_name", "address", "phone"])
+        if not cleaned_data["for_other"] \
+                and not cleaned_data["new_client"]:
+            self.required_fields(["this_order_client"])
+        return self.cleaned_data
+
+    def required_fields(self, fields):
+        for field in fields:
+            if not self.cleaned_data.get(field, ""):
+                msg = forms.ValidationError(
+                    "Это поле обязательно!")
+                self.add_error(field, msg)
 
 
 class OrderChangeForm(forms.ModelForm):
     class Meta:
         model = Order
-        fields = ['status', 'description', 'this_order_client', 'full_name']
+        fields = ("full_name", "address", "phone",
+                  "status", "description")
 
-    full_name = forms.CharField(disabled=True)
-    this_order_client = forms.CharField(disabled=True)
-    status = forms.Select(choices=Order.STATUS,)
-    description = forms.CharField(max_length=500,
-                                  required=False,
-                                  error_messages=
-                                  {'invalid': 'Макс. длинна = 500 символов'},
-                                  widget=forms.Textarea
-                                  (attrs={'width': "50%", 'cols': "60", 'rows': "10", }))
+    full_name = forms.CharField(
+        label="",
+        help_text="",
+        min_length=3,
+        max_length=32,
+        required=False,
+        widget=forms.TextInput(
+            attrs={"class": "form-control new-fullname",
+                   "placeholder": "Имя получателя"}))
+
+    address = forms.CharField(
+        label="",
+        help_text="",
+        min_length=8,
+        max_length=250,
+        required=False,
+        widget=forms.TextInput(
+            attrs={"class": "form-control new-address",
+                   "placeholder": "Адрес доставки"}))
+
+    phone = forms.CharField(
+        label="",
+        help_text="",
+        required=False,
+        validators=[
+            RegexValidator(
+                regex=r"^\+?1?\d{9,16}$",
+                message="Телефон должен быть в формате: +700112299"
+                        " Макс. длинна = 16 знаков")],
+        widget=forms.TextInput(
+            attrs={"class": "form-control new-phone",
+                   "placeholder": "Телефон получателя"}))
+
+    status = forms.Select(choices=Order.STATUS)
+
+    description = forms.CharField(
+        label="",
+        help_text="",
+        max_length=500,
+        required=False,
+        widget=forms.Textarea(
+            attrs={"class": "form-control",
+                   "placeholder": "Другие примечания / пожелания",
+                   "width": "50%", "cols": "60",
+                   "rows": "10", }))
+
+    def clean_status(self):
+        data = self.cleaned_data["status"]
+        if isinstance(data, int) and data <= 5:
+            return data
+        else:
+            raise forms.ValidationError(
+                "Значением статуса может быть "
+                "только число в пределах 1-5!")
