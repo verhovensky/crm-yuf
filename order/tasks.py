753517e64ff293fdbf30518f19dedbdff96fbcf3
verhovensky <voinywolnyprod@gmail.com>
2022-08-31 14:34:18 +0300

    tasks.py minor fixes

--- order/tasks.py
+++ order/tasks.py
@@ -1,14 +1,6 @@
 from __future__ import absolute_import, unicode_literals
 from .utils import make_status_expired
-
 from crmdev.my_celery import app
-
-
-@app.task(queue='order')
-def mynumsadd(**kwargs):
-    print('TASK EXECUTE mynumsadd')
-    res = kwargs['x'] + kwargs['y']
-    return {'result':res}
 
 
 @app.task(queue='order')
@@ -17,7 +9,6 @@ def expire_order(**kwargs):
     All checks performed in make_status_expired function
     All other ORM related functions in utils.py of this app
     :param order_id """
-
     id = kwargs['order_id']
     result = make_status_expired(order_id=id)
     return result
