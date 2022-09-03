bbd6a80baca01410d03ebb68cac0c1b936e898f4
verhovensky <voinywolnyprod@gmail.com>
2022-09-03 21:09:57 +0300

    admin.py refactor

--- account/admin.py
+++ account/admin.py
@@ -1,13 +1,15 @@
 from django.contrib import admin
-from .models import UserProfile
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 
 class ProfileAdmin(admin.ModelAdmin):
-    list_display = ('user',
-                    'date_of_birth',
+    list_display = ('username',
+                    'first_name',
                     'phone_number',
                     'closed_sales',
                     'sales_amount')
 
 
-admin.site.register(UserProfile, ProfileAdmin)
+admin.site.register(User, ProfileAdmin)
