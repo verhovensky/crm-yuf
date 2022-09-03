1ed5777cc33b831613f3f978773efc6072b2dfd8
verhovensky <voinywolnyprod@gmail.com>
2022-09-03 21:08:22 +0300

    forms.py refactor

--- account/forms.py
+++ account/forms.py
@@ -1,35 +1,75 @@
-from .models import UserProfile
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
 from bootstrap_datepicker_plus.widgets import DatePickerInput
+from account.models import phone_regex
 from django import forms
+
+User = get_user_model()
 
 
 class RegisterUserForm(UserCreationForm):
     class Meta:
         model = User
-        fields = ['username', 'email',
-                  'password1', 'password2']
+        fields = ("username", "email",
+                  "password1", "password2")
 
 
 class UserEditForm(forms.ModelForm):
+
     class Meta:
         model = User
-        fields = ['first_name', 'last_name', 'email']
+        fields = ("first_name", "last_name", "email",
+                  "date_of_birth", "phone_number", "photo")
 
+    date_of_birth = forms.DateField(
+        label="Дата рождения",
+        help_text="",
+        widget=DatePickerInput(
+            attrs={"class": "form-control datepicker-input",
+                   "data-target": "#datebirth",
+                   "placeholder": "Дата рождения",
+                   "format": "%m/%d/%Y"}),
+        input_formats=("%m/%d/%Y",),
+        required=False)
 
-class ProfileEditForm(forms.ModelForm):
+    first_name = forms.CharField(
+        label="",
+        help_text="",
+        max_length=150,
+        required=False,
+        widget=forms.TextInput(
+            attrs={"class": "form-control first_name",
+                   "placeholder": "Имя"}))
 
-    class Meta:
-        model = UserProfile
-        fields = ['date_of_birth', 'phone_number', 'photo']
+    last_name = forms.CharField(
+        label="",
+        help_text="",
+        max_length=150,
+        required=False,
+        widget=forms.TextInput(
+            attrs={"class": "form-control last_name",
+                   "placeholder": "Фамилия"}))
 
-    date_of_birth = forms.DateField(label="Дата рождения",
-                                    help_text="",
-                                    widget=DatePickerInput
-                                    (attrs={'class': 'form-control datepicker-input',
-                                            'data-target': '#datebirth',
-                                            'placeholder': 'Дата рождения',
-                                            'format': '%m/%d/%Y'}),
-                                    input_formats=['%m/%d/%Y'],
-                                    required=False)
+    email = forms.EmailField(
+        label="",
+        help_text="",
+        max_length=150,
+
+        widget=forms.TextInput(
+            attrs={"class": "form-control last_name",
+                   "readonly": True}))
+
+    phone_number = forms.CharField(
+        label="",
+        help_text="",
+        max_length=16,
+        validators=(phone_regex,),
+        widget=forms.TextInput(
+            attrs={"class": "form-control phone_number",
+                   "placeholder": "Мобильный телефон"}))
+
+    photo = forms.FileField(
+        label="",
+        help_text="",
+        widget=forms.FileInput(
+            attrs={"class": "form-file"}))
