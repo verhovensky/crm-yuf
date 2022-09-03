7eed0b0b4da208b12a2f34f8bf1c734a10ee0034
verhovensky <voinywolnyprod@gmail.com>
2022-09-03 21:07:55 +0300

    apps.py refactor, app name fix, signals remove

--- account/apps.py
+++ account/apps.py
@@ -3,7 +3,4 @@ from django.apps import AppConfig
 
 class ProfilesConfig(AppConfig):
     name = 'account'
-    verbose_name = 'профиль'
-
-    def ready(self):
-        import account.signals
+    verbose_name = 'персонал'
