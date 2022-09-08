da1b3d01f5455d5eae091e62ecdac0d8394d1b02
verhovensky <voinywolnyprod@gmail.com>
2022-09-08 17:20:22 +0300

    forms.py quantity field attrs fix

--- cart/forms.py
+++ cart/forms.py
@@ -4,9 +4,19 @@ from django.core.validators import MinValueValidator
 
 
 class CartAddProductForm(forms.Form):
-    quantity = forms.DecimalField(validators=[MinValueValidator(Decimal('1.0'))],
-                                  max_digits=10,
-                                  label='Количество')
-    update = forms.BooleanField(required=False,
-                                initial=False,
-                                widget=forms.HiddenInput)
+
+    # TODO: not 0 quantity, not - quantity
+
+    quantity = forms.DecimalField(
+        validators=[MinValueValidator(Decimal('1.0'))],
+        max_digits=10,
+        label="",
+        widget=forms.NumberInput(
+            attrs={"class": "text-center form-control",
+                   "style": "max-width: 19em;",
+                   "min": "1.0",
+                   "placeholder": "Добавить в заказ"}))
+    update = forms.BooleanField(
+        required=False,
+        initial=False,
+        widget=forms.HiddenInput)
