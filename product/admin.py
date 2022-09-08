d7494cd6f677188e1e712f837166fe57223d5fda
verhovensky <voinywolnyprod@gmail.com>
2022-09-08 17:01:42 +0300

    admin.py refactor

--- product/admin.py
+++ product/admin.py
@@ -1,21 +1,22 @@
 from django.contrib import admin
 from .models import Category, Product
+from django_mptt_admin.admin import DjangoMpttAdmin
 
 
-class CategoryAdmin(admin.ModelAdmin):
-    list_display = ['name', 'slug']
-    prepopulated_fields = {'slug': ('name',)}
+class CategoryAdmin(DjangoMpttAdmin):
+    prepopulated_fields = {"slug": ("name",)}
 
 
 admin.site.register(Category, CategoryAdmin)
 
 
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['name', 'slug', 'price', 'stock', 'available',
-                    'calc_total_product', 'created', 'updated', 'created_by']
-    list_filter = ['available', 'created', 'updated']
-    list_editable = ['price', 'stock', 'available']
-    prepopulated_fields = {'slug': ('name',)}
+    list_display = ["name", "slug", "price", "stock", "available",
+                    "calc_total_product", "created",
+                    "updated", "created_by"]
+    list_filter = ["available", "created", "updated"]
+    list_editable = ["price", "stock", "available"]
+    prepopulated_fields = {"slug": ("name",)}
 
 
 admin.site.register(Product, ProductAdmin)
